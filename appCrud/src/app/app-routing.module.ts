cc9a06126a32e3336f487816c0a4f464829eb224
savegnago <savegdesigner@gmail.com>
2020-06-09 20:04:50 -0300

    categorias init

--- appCrud/src/app/app-routing.module.ts
+++ appCrud/src/app/app-routing.module.ts
@@ -2,11 +2,12 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { AboutComponent } from './views/about/about.component';
 import { GameComponent } from './views/game/game.component';
-
+import { CategoriasComponent } from './views/categorias/categorias.component';
 
 const routes: Routes = [
   { path: 'about', component: AboutComponent },
   { path: 'game', component: GameComponent },
+  { path: 'categorias', component: CategoriasComponent}
 ];
 
 @NgModule({
---
+++ appCrud/src/app/views/categorias/categorias.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit } from '@angular/core';
+
+export interface PeriodicElement {
+  name: string;
+  position: number;
+  weight: number;
+  symbol: string;
+}
+
+const ELEMENT_DATA: PeriodicElement[] = [
+  {position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H'},
+  {position: 2, name: 'Helium', weight: 4.0026, symbol: 'He'},
+  {position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li'},
+];
+
+@Component({
+  selector: 'app-categorias',
+  templateUrl: './categorias.component.html',
+  styleUrls: ['./categorias.component.css']
+})
+export class CategoriasComponent implements OnInit {
+
+  displayedColumns: string[] = ['id', 'name', 'editar', 'deletar'];
+  dataSource = ELEMENT_DATA;
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
